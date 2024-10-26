aaef7b76c0f2264a4e267c8d1a43fdc52c12f95b
Soje <josefrank1905@hotmail.com>
2024-10-26 15:21:19 -0500

    crud sesion

--- app/Http/Controllers/sesion_controller.php
+++ app/Http/Controllers/sesion_controller.php
@@ -59,4 +59,138 @@ class sesion_controller extends Controller
      ];
      return response()->json($data,201);
     }
+
+    public function show($IDSESION){
+        $sesion = Sesion::find($IDSESION);
+        if(!$sesion){
+            $data =[
+                'message' => 'Sesion no encontrada',
+                'status' => 404
+            ];
+            return response()->json($data,404);
+        }
+        $data = [
+            'sesion' => $sesion,
+            'status' => 200
+        ];
+        return response()->json($data,200);
+    }
+
+    public function delete($IDSESION){
+        $sesion = Sesion::find($IDSESION);
+        if(!$sesion){
+            $data = [
+                'message' => 'Sesion no encontrada',
+                'status' => 404
+            ];
+            return response()->json($data,404);
+        }
+        $sesion->delete();
+        $data = [
+            'message' => 'Sesión eliminada',
+            'status' => 200
+        ];
+        return response()->json($data,200);
+    }
+
+    public function update(Request $request, $IDSESION){
+        $sesion = sesion::find($IDSESION);
+        if(!$sesion){
+            $data = [
+                'message' => 'Sesión no encontrada',
+                'status' => 404
+            ];
+            return response()->json($data,404);
+        }
+        $validator = Validator::make($request->all(),[
+            'LUGAR' => 'required',
+            'FECHA' => 'required|date',
+            'HORARIO_INICIO' => 'required|date_format:H:i',
+            'HORARIO_FINAL' => 'required|date_format:H:i',
+            'PRESIDENTE' => 'required',
+            'SECRETARIO' => 'required',
+         ]);
+         if($validator->fails()){
+            $data = [
+              'message' => 'Error en la validación de los datos',
+              'errors' => $validator->errors(),
+              'status' => 400
+            ];
+            return response()->json($data,400);
+
+            }
+            $sesion->LUGAR = $request ->LUGAR;
+            $sesion->FECHA = $request ->FECHA;
+            $sesion->HORARIO_INICIO = $request->HORARIO_INICIO;
+            $sesion->HORARIO_FINAL = $request ->HORARIO_FINAL;
+            $sesion->PRESIDENTE = $request-> PRESIDENTE;
+            $sesion->SECRETARIO = $request-> SECRETARIO;
+            
+                $sesion->save();
+
+                $data = [
+                    'message' => 'Sesion actualizada',
+                    'sesion' => $sesion,
+                    'status' => 200
+                ];
+                return response()->json($data,200);
+    }
+
+    public function update_patch(Request $request, $IDSESION){
+        $sesion = Sesion::find($IDSESION);
+
+        if(!$sesion){
+            $data = [
+                'message'=> 'Sesion no encontrada',
+                'status' => 404
+            ];
+            return response()->json($data,404);
+        }
+        
+        $validator = Validator::make($request->all(),[
+            'LUGAR' => 'max:255',
+            'FECHA' => 'date',
+            'HORARIO_INICIO' => 'date_format:H:i',
+            'HORARIO_FINAL' => 'date_format:H:i',
+            'PRESIDENTE' => 'max:255',
+            'SECRETARIO' => 'max:255'
+        ]);
+
+        if($validator->fails()){
+            $data = [
+              'message' => 'Error en la validación de los datos',
+              'errors' => $validator->errors(),
+              'status' => 400
+            ];
+            return response()->json($data,400);
+
+            }
+            if($request->has('LUGAR')){
+                $sesion->LUGAR = $request->LUGAR;
+            }
+            if($request->has('FECHA')){
+                $sesion->FECHA = $request->FECHA;
+            }
+            if($request->has('HORARIO_INICIO')){
+                $sesion->HORARIO_INICIO = $request->HORARIO_INICIO;
+            }
+            if($request->has('HORARIO_FINAL')){
+                $sesion->HORARIO_FINAL = $request->HORARIO_FINAL;
+            }
+            if($request->has('PRESIDENTE')){
+                $sesion->PRESIDENTE = $request->PRESIDENTE;
+            }
+            if($request->has('SECRETARIO')){
+                $sesion->SECRETARIO = $request->SECRETARIO;
+            }
+
+            $sesion->save();
+
+            $data = [
+                'message'=> 'Sesion actualizada',
+                'sesion' => $sesion,
+                'status' =>200
+            ];
+            return response()->json($data,200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,14 @@ Route::get('/user', function (Request $request) {
 //CRUD sesion
 Route::get('/sesion',[sesion_controller::class, 'index']);
 
+Route::get('/sesion/{IDSESION}',[sesion_controller::class, 'show']); //encontrar sesion por id
+
 Route::post('/sesion',[sesion_controller::class, 'store']);
+
+Route::delete('/sesion/{IDSESION}', [sesion_controller::class, 'delete']);
+
+Route::put('/sesion/{IDSESION}', [sesion_controller::class, 'update']);
+
+
+Route::patch('/sesion/{IDSESION}', [sesion_controller::class, 'update_patch']);
+
